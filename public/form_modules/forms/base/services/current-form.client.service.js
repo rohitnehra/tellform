d69f300128ee0202cdb32fbada7a233b90a31472
Rohit Nehra <31066431+rohitnehra@users.noreply.github.com>
2019-04-18 20:32:06 +0530

    Add files via upload

---
+++ public/form_modules/forms/base/services/current-form.client.service.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Forms service used for communicating with the forms REST endpoints
+angular.module('view-form').service('CurrentForm',
+	function(){
+		//Private variables
+		var _form = {};
+
+		//Public Methods
+		this.getForm = function() {
+	        return _form;
+	    };
+	    this.setForm = function(form) {
+	        _form = form;
+	    };
+    }
+);
---
+++ public/form_modules/forms/forms.client.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Use Application configuration module to register a new module
+ApplicationConfiguration.registerModule('view-form', [
+	'ngFileUpload', 'ui.date', 'angular-input-stars'
+]);
